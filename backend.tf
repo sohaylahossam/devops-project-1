959e2364fb4f08ad556dd9b7a5088c69ef8071db
sohaylahossam <sohaylahossamm@gmail.com>
2025-11-07 20:11:48 +0200

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket       = "hello-devops-production-terraform-state"
+    bucket       = "cicdpipelinetestdepi"
     key          = "eks/terraform.tfstate"
     region       = "eu-west-1"
     encrypt      = true
