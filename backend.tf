2c39ca40a66643b225a8530f34e62b2929b8434f
sohaylahossam <sohaylahossamm@gmail.com>
2025-11-07 20:12:15 +0200

    Change Terraform backend region to us-east-1

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket       = "cicdpipelinetestdepi"
     key          = "eks/terraform.tfstate"
-    region       = "eu-west-1"
+    region       = "us-east-1"
     encrypt      = true
     use_lockfile = true
   }
